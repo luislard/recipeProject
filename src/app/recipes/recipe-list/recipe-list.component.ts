77b86cdc8e25b89e38f9bdecfe6642aa2703767c
Luis Rosales <eng.luisrosales@gmail.com>
2017-10-09 22:10:08 +0200

    harcoding html to render one recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test','http://maxpixel.freegreatpicture.com/Meat-Power-Recipe-Food-Dishes-Pork-1459693'),
+  ];
   constructor() { }
 
   ngOnInit() {
